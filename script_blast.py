7176743d438a568062359a313326fb0d3cb01ec3
grupo14 <gabrielpeixoto@sapo.pt>
2014-12-20 18:58:07 +0000

    Update script_blast.py

--- script_blast.py
+++ script_blast.py
@@ -5,25 +5,16 @@ from Bio import SeqIO
 from Bio.Blast import NCBIWWW
 
 seq_record = SeqIO.read('genes.gb', 'gb')
-translated = seq_record.seq.translate(table='Bacterial')
 proteins = []
-protsAtuais = []
-for aa in translated:
-    if aa == "_":
-        if protsAtuais:
-            for p in protsAtuais:
-                proteins.append(SeqRecord(Seq(p, IUPAC.IUPACProtein)))
-            protsAtuais = []
-        else:
-            if aa == "M":
-                protsAtuais.append("")
-            for i in xrange(len(protsAtuais)):
-                protsAtuais[i] += aa
-for j in xrange(len(proteins)):
-    record = SeqIO.read(proteins[j], format == 'gb')
-    result_handle = NCBIWWW.qblast('blastp', 'nr', record.format('gb'))
-    name_protein = 'blast_gene_' + str(j) + '.xml'
-    save_file = open(name_protein, 'w')
+for feature in seq_record.features:
+    if feature.type == "CDS":
+        seq_protein = Seq(str(feature.qualifiers['translation']), IUPAC.extended_protein)
+        protein_record = SeqRecord(seq_protein)
+        proteins.append(protein_record)
+save_file = open('blast.xml', 'w')
+for protein in proteins:
+    result_handle = NCBIWWW.qblast('blastp', 'nr', protein.format('gb'))
     save_file.write(result_handle.read())
-    save_file.close()
-    result_handle.close()
+    save_file.write('\n')
+save_file.close()
+result_handle.close()
