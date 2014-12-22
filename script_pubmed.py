a844c9a87fc072b88be4ac78766a423e59aa948f
grupo14 <gabrielpeixoto@sapo.pt>
2014-12-22 17:28:32 +0000

    Update script_pubmed.py

--- script_pubmed.py
+++ script_pubmed.py
@@ -14,23 +14,24 @@ from Bio import SeqIO
 
 seq_record = SeqIO.read('genes.gb', 'gb')
 Entrez.email = "gabrielpeixoto@sapo.pt"#o e-mail é apenas um exemplo
-for feature in seq_record.features:
-    if feature.type == "CDS":
-        title = str(feature.qualifiers["db_xref"][1].strip("GeneID:"))#pesquisa por gene da sequência
-        handle = Entrez.egquery(term = title)
-        record = Entrez.read(handle)
-        number = 0
-        for row in record["eGQueryResult"]:
-            if row["DbName"]=="pubmed":
-                number = row["Count"]
-        handle = Entrez.esearch(db = "pubmed", term = "Legionella  pneumophila", retmax=number)
-        record = Entrez.read(handle)
-        idlist = record["IdList"]
-        handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
-        records = list(Medline.parse(handle))
-        record_results = open('artigos.txt', 'w')
-        for record in records:
-            texts = "title: " + str(record.get("TI", "?")) + "\n" + "authors: " + str(record.get("AU", "?")) + "\n" + "source: " + str(record.get("SO", "?"))
-            record_results.write(texts)
-            record_results.write("\n")
+record_results = open('artigos.txt', 'w')
+handle = Entrez.egquery(term = "Legionella pneumophila")
+record = Entrez.read(handle)
+number = 0
+for row in record["eGQueryResult"]:
+    if row["DbName"]=="pubmed":
+        number = row["Count"]
+handle = Entrez.esearch(db = "pubmed", term = "Legionella  pneumophila philadelphia 1", retmax=number)
+record = Entrez.read(handle)
+idlist = record["IdList"]
+handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
+records = list(Medline.parse(handle))
+for record in records:
+    record_results.write("title: ")
+    record_results.write(str(record.get("TI", "?")))
+    record_results.write("\nauthors: ")
+    record_results.write(str(record.get("AU", "?")))
+    record_results.write("\nsource: ")
+    record_results.write(str(record.get("SO", "?")))
+    record_results.write("\n\n")
 record_results.close()
