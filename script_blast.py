1c62ba15f07f06942d5956d91430de69efe43ead
grupo14 <gabrielpeixoto@sapo.pt>
2015-01-03 16:50:20 +0000

    Update script_blast.py

--- script_blast.py
+++ script_blast.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on Fri Dec 19 14:13:38 2014
 
@@ -7,30 +8,35 @@ Created on Fri Dec 19 14:13:38 2014
 #script para correr o blast, com as proteínas resultantes da sequência selecionada como querys
 
 #importações
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML 
 
 #registo do GI das proteinas resultantes da sequência selecionada
 seq_record = SeqIO.read('genes.gb', 'gb')
-GIs = []
+proteins = []
 for feature in seq_record.features:
     if feature.type == "CDS":
-        GI = str(feature.qualifiers['db_xref'][0])[-3:]
-        GIs.append(GI)
+        seq_protein = Seq(str(feature.qualifiers['translation']), IUPAC.extended_protein)
+        protein_record = SeqRecord(seq_protein)
+        proteins.append(protein_record)
 
 #execução do blast
 save_file = open('blast.xml', 'w')
-for GI in GIs:
-    result_handle = NCBIWWW.qblast('blastp', 'swissprot', GI.format('GI'))
+for protein in proteins:
+    result_handle = NCBIWWW.qblast('blastp', 'swissprot', protein.format('gb'))
     save_file.write(result_handle.read())
+    save_file.write('\n\n')
 save_file.close()
 result_handle.close()
 
 #verificação dos resultados (baseado no código desenvolvido pelo grupo 1)
 verify = open('blast_verificacao.txt', 'w')
 E_VALUE_THRESH = 0.1
-result = open(blast.xml)
+result = open('blast.xml','r')
 records = NCBIXML.parse(result)
 for record in records:
     for alignment in record.alignments:
@@ -43,4 +49,5 @@ for record in records:
                 verify.write(hsp.query[0:75] + '...')
                 verify.write(hsp.match[0:75] + '...')
                 verify.write(hsp.sbjct[0:75] + '...')
+                verify.write('\n')
 verify.close()
