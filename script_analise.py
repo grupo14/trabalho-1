33a4f1d15d5e28fffc71873dc10179498c2259e5
grupo14 <gabrielpeixoto@sapo.pt>
2015-01-05 18:20:19 +0000

    Update script_analise.py

--- script_analise.py
+++ script_analise.py
@@ -32,76 +32,59 @@ for code in codes:
          try:
              record = SwissProt.read(data)
              for ref in record.references:
-                 f.write("\n****Codigo %s****\n" %code)
-                 f.write("\n****Informacao sobre a proteina****\n")
-                 f.write("\n\nNome:%s\n" %record.entry_name)
-                 f.write("\nClasse:%s\n" %record.data_class)
-                 f.write("\nTipo de molecula:%s\n" %record.molecule_type)
-                 f.write("\nTamanho da sequencia:%s\n" %record.sequence_length)
-                 f.write("\nAccession:%s\n" %record.accessions)
-                 f.write("\nCriado:%s\n"% str(record.created))
-                 f.write("\nAdaptacao da sequencia:%s\n" %str(record.sequence_update))
-                 f.write("\nAdaptacao da anotacao:%s\n" %str(record.annotation_update))
-                 f.write("\nDescricao:%s\n" %record.description)
-                 f.write("\nNome do gene:%s\n" %record.gene_name)
-                 f.write("\nOrganismo:%s\n" %record.organism)
-                 f.write("\nOrganelo:%s\n" %record.organelle)
-                 f.write("\nClassificacao do Organismo:%s\n" %record.organism_classification)
-                 f.write("\nID da taxonomia:%s\n" %record.taxonomy_id)
-                 f.write("\nOrganismo Hospedeiro:%s\n" %record.host_organism)
-                 f.write("\nID da taxonomia do hospedeiro:%s\n" %record.host_taxonomy_id)
-                 f.write("\nReferencias:%s\n" %record.references)
-                 f.write("\nComentarios:%s\n" %record.comments)
-                 f.write("\nReferencias cruzadas:%s\n" %record.cross_references)
-                 f.write("\nPalavras-chave:%s\n" %record.keywords)
-                 f.write("\nCaracteristicas:%s\n" %record.features)
-                 f.write("\nInformacao sobre a sequencia:%s\n" %str(record.seqinfo))
-                 f.write("\nSequencia:%s\n" %str(record.sequence))
+                 f.write("\n\n****Informacao sobre a proteina %s****\n" %code)
+                 f.write("\n\nNome: %s\n" %record.entry_name)
+                 f.write("\nClasse: %s\n" %record.data_class)
+                 f.write("\nTipo de molecula: %s\n" %record.molecule_type)
+                 f.write("\nTamanho da sequencia: %s\n" %record.sequence_length)
+                 f.write("\nCodigo de Accesso: %s\n" %record.accessions)
+                 f.write("\nCriado: %s\n"% str(record.created))
+                 f.write("\nAdaptacao da sequencia: %s\n" %str(record.sequence_update))
+                 f.write("\nAdaptacao da anotacao: %s\n" %str(record.annotation_update))
+                 f.write("\nDescricao: %s\n" %record.description)
+                 f.write("\nNome do gene: %s\n" %record.gene_name)
+                 f.write("\nOrganismo: %s\n" %record.organism)
+                 f.write("\nOrganelo: %s\n" %record.organelle)
+                 f.write("\nClassificacao do Organismo: %s\n" %record.organism_classification)
+                 f.write("\nID da taxonomia: %s\n" %record.taxonomy_id)
+                 f.write("\nOrganismo Hospedeiro: %s\n" %record.host_organism)
+                 f.write("\nID da taxonomia do hospedeiro: %s\n" %record.host_taxonomy_id)
+                 f.write("\nReferencias: %s\n" %record.references)
+                 f.write("\nComentarios: %s\n" %record.comments)
+                 f.write("\nReferencias cruzadas: %s\n" %record.cross_references)
+                 f.write("\nPalavras-chave: %s\n" %record.keywords)
+                 f.write("\nCaracteristicas: %s\n" %record.features)
+                 f.write("\nInformacao sobre a sequencia: %s\n" %str(record.seqinfo))
+                 f.write("\nSequencia: %s\n" %str(record.sequence))
                  f.write("\n****Referencias sobre a proteina****\n")
-                 f.write("\nNumero:%s\n" %ref.number)
-                 f.write("\nPosicao:%s\n" %ref.positions)
-                 f.write("\nComentarios:%s\n" %ref.comments)
-                 f.write("\nReferencias:%s\n" %ref.references)
-                 f.write("\nAutores:%s\n" %ref.authors)
-                 f.write("\nTitulo:%s\n" %ref.title)
-                 f.write("\nLocalizacao:%s\n" %ref.location)
+                 f.write("\nNumero: %s\n" %ref.number)
+                 f.write("\nPosicao: %s\n" %ref.positions)
+                 f.write("\nComentarios: %s\n" %ref.comments)
+                 f.write("\nReferencias: %s\n" %ref.references)
+                 f.write("\nAutores: %s\n" %ref.authors)
+                 f.write("\nTitulo: %s\n" %ref.title)
+                 f.write("\nLocalizacao: %s\n\n" %ref.location)
              break
          except Exception:
              break
 f.close()
 
-#análise da estrutura das proteínas relevantes com base no PDB (código baseado no desenvolvido pelo grupo 10)
+#análise da estrutura das proteínas relevantes com base nos ficheiros PDB encontrados (código baseado no desenvolvido pelo grupo 10)
 parser = PDBParser()
-a = "SIM"
-while a == "SIM":
-    s = raw_input ("Nome do ficheiro a analisar:")
-    ficheiro= open("Resultados da %s"%s, "w")
-    structure = parser.get_structure('%s'%s, '%s.pdb'%s)
-    pdbl = PDBList()
-    pdbl.retrieve_pdb_file('%s'%s)
-    print "\nAnalise do ficheiro: %s"%(s)
-    keywords = structure.header['keywords']
-    ficheiro.write(keywords)
-    print "\nPalavras Chave:%s"%keywords
-    name = structure.header['name']
-    print "\nNome do Organismo:%s"%name
-    head = structure.header['head']
-    print "\nCabecalho:%s"%head
-    dep = structure.header['deposition_date']
-    print "\nData da deposicao::%s"%dep
-    dat = structure.header['release_date']
-    print "\nData da publicacaos::%s"%dat
-    s = structure.header['structure_method']
-    print "\nMetodo usado:%s"%s
-    resolution = structure.header['resolution']
-    print "\nResolucao:%s"%resolution
-    ref = structure.header['structure_reference']
-    print "\nReferencia da estrutura:%s"%ref
-    j = structure.header['journal_reference']
-    print "\nReferencia de artigo:%s"%j
-    a = structure.header['author']
-    print "\nAutor:%s"%a
-    c = structure.header['compound']
-    print "\nComposto:%s\n"%c
-    w = raw_input ("Existe mais algum ficheiro que deseje analisar( responda sim ou nao):")
-    a = w.upper()
+ficheiro= open("analise_pdb.txt", "w")
+structure = parser.get_structure('4F67', '4F67.pdb')
+pdbl = PDBList()
+pdbl.retrieve_pdb_file('4F67')
+ficheiro.write("****Analise do ficheiro 4F67.pdb****\n")
+ficheiro.write("\nPalavras Chave: %s\n" %structure.header['keywords'])
+ficheiro.write("\nNome do Organismo: %s\n" %structure.header['name'])
+ficheiro.write("\nCabecalho: %s" %structure.header['head'])
+ficheiro.write("\nData da deposicao: %s\n" %structure.header['deposition_date'])
+ficheiro.write("\nData da publicacaos: %s\n" %structure.header['release_date'])
+ficheiro.write("\nMetodo usado: %s\n" %structure.header['structure_method'])
+ficheiro.write("\nResolucao: %s\n" %structure.header['resolution'])
+ficheiro.write("\nReferencia da estrutura: %s\n" %structure.header['structure_reference'])
+ficheiro.write("\nReferencia de artigo: %s\n" %structure.header['journal_reference'])
+ficheiro.write("\nAutor: %s\n" %structure.header['author'])
+ficheiro.write("\nComposto: %s" %structure.header['compound'])
+ficheiro.close()
